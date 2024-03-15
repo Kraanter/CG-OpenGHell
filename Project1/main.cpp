6a31f1b4105e7b31723676adb2790594cdcf8ac0
Kraanter <krijngrimme@hotmail.com>
2024-03-15 15:32:30 +0100

    Added teapoit

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <GL/glew.h>
+#include <vector>
 #include <GL/freeglut.h>
 
 #include <glm/glm.hpp>
@@ -8,6 +9,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "glsl.h"
+#include "objloader.h"
 
 using namespace std;
 
@@ -41,85 +43,12 @@ glm::mat4 view;
 glm::mat4 projection;
 glm::mat4 mvp;
 
+vector<glm::vec3> vertices;
+vector<glm::vec2> uvs;
+vector<glm::vec3> normals;
+
 // animation
 bool animate = false;
-
-//--------------------------------------------------------------------------------
-// Mesh variables
-//--------------------------------------------------------------------------------
-//------------------------------------------------------------
-// Variables for object
-//
-//           7----------6
-//          /|         /|
-//         / |        / |
-//        /  4-------/--5               y
-//       /  /       /  /                |
-//      3----------2  /                 ----x
-//      | /        | /                 /
-//      |/         |/                  z
-//      0----------1
-//------------------------------------------------------------
-
-GLfloat vertices[] = {
-    // front
-    -1.0, -1.0, 1.0,
-    1.0, -1.0, 1.0,
-    1.0, 1.0, 1.0,
-    -1.0, 1.0, 1.0,
-    // back
-    -1.0, -1.0, -1.0,
-    1.0, -1.0, -1.0,
-    1.0, 1.0, -1.0,
-    -1.0, 1.0, -1.0
-};
-
-GLfloat colors[] = {
-    // front colors
-    1.0, 1.0, 0.0,
-    0.0, 1.0, 0.0,
-    0.0, 0.0, 1.0,
-    1.0, 1.0, 1.0,
-    // back colors
-    0.0, 1.0, 1.0,
-    1.0, 0.0, 1.0,
-    1.0, 0.0, 0.0,
-    1.0, 1.0, 0.0
-};
-
-GLushort cube_elements[] = {
-    // 0,1,1,2,2,3,3,0,  // front
-    // 0,4,1,5,3,7,2,6,  // front to back
-    // 4,5,5,6,6,7,7,4   //back
-    0, 1, 2,
-    0, 3, 2,
-    0, 4, 5,
-    0, 5, 1,
-    1, 5, 6,
-    1, 6, 2,
-    3, 4, 0,
-    3, 7, 4,
-    4, 6, 5,
-    4, 6, 7,
-    2, 7, 3,
-    2, 6, 7,
-};
-
-
-// // Vertices
-// const GLfloat vertices[] = {
-//     0.5, -0.5, 0.0, 1,
-//     -0.5, -0.5, 0.0, 1,
-//     0.0, 0.5, 0.0, 1
-// };
-//
-// // Colors
-// const GLfloat colors[] = {
-//     1.0f, 0.0f, 0.0f,
-//     0.0f, 1.0f, 0.0f,
-//     0.0f, 0.0f, 1.0f
-// };
-
 
 //--------------------------------------------------------------------------------
 // Keyboard handling
@@ -163,7 +92,7 @@ void Render()
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     if (animate)
-        model = glm::rotate(model, 0.01f, glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::rotate(model, 0.1f, glm::vec3(0.0f, 1.0f, 0.0f));
     mvp = projection * view * model;
     
     // Attach to program_id
@@ -174,9 +103,7 @@ void Render()
 
     // Send vao
     glBindVertexArray(vao);
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
-    // glDrawElements(GL_LINES, sizeof(cube_elements) / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
-    glDrawElements(GL_TRIANGLES, sizeof(cube_elements) / sizeof(GLushort), GL_UNSIGNED_SHORT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
     glBindVertexArray(0);
 
     // Swap buffers
@@ -244,37 +171,20 @@ void InitShaders()
 void InitBuffers()
 {
     GLuint position_id;
-    GLuint color_id;
     GLuint vbo_vertices;
-    GLuint vbo_colors;
-    GLuint ibo_cube_elements;
 
     // vbo for vertices
     glGenBuffers(1, &vbo_vertices);
     glBindBuffer(GL_ARRAY_BUFFER, vbo_vertices);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // vbo for colors
-    glGenBuffers(1, &vbo_colors);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_colors);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    
-    // ibo for elements
-    glGenBuffers(1, &ibo_cube_elements);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cube_elements);
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_elements), cube_elements, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // Get vertex attributes
     position_id = glGetAttribLocation(program_id, "position");
-    color_id = glGetAttribLocation(program_id, "color");
 
-    // Allocate memory for vao
+    // Allocate vao
     glGenVertexArrays(1, &vao);
 
-    // Bind to vao
+    // Bind vao
     glBindVertexArray(vao);
 
     // Bind vertices to vao
@@ -283,19 +193,9 @@ void InitBuffers()
     glEnableVertexAttribArray(position_id);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    // Bind colors to vao
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_colors);
-    glVertexAttribPointer(color_id, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(color_id);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // bind ibo to vao
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo_cube_elements);
-    
-    // Stop bind to vao
     glBindVertexArray(0);
 
-    // univorm_mvp = glGetUniformLocation(program_id, "mvp");
+    univorm_mvp = glGetUniformLocation(program_id, "mvp");
 }
 
 void InitMatricies()
@@ -307,8 +207,8 @@ void InitMatricies()
     // model = glm::rotate(model, glm::radians(45.0f), glm::vec3(1.0f, 0.0f, 1.0f));
 
     view = glm::lookAt(
-        glm::vec3(2.0f, 2.0f, 7.0f),
-        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(0.0f, 2.0f, 4.0f),
+        glm::vec3(0.0f, 0.5f, 0.0f),
         glm::vec3(0.0f, 1.0f, 0.0f)
     );
 
@@ -322,10 +222,16 @@ void InitMatricies()
     mvp = projection * view * model;
 }
 
+void InitObjects()
+{
+    bool res = loadOBJ("objects/teapot.obj", vertices, uvs, normals);
+}
+
 
 int main(int argc, char** argv)
 {
     InitGlutGlew(argc, argv);
+    InitObjects();
     InitShaders();
     InitMatricies();
     InitBuffers();
